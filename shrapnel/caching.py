2429b30c9d2b98667e286f44b281590848ebdb48
Kurtiss Hare <kurtiss@gmail.com>
2010-02-25 19:10:34 -0800

    Fixes cache key encoding

--- shrapnel/caching.py
+++ shrapnel/caching.py
@@ -26,6 +26,9 @@ def cached():
 			
 			if not hasattr(handler, '_cache'):
 				handler._cache = {}
+				
+			if isinstance(s, unicode):
+				cache_key = cache_key.encode('ascii')
 			
 			result = handler._cache.get(cache_key, None)
 
