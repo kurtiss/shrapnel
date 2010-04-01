6115e5e61bccb2f03ac730461d0a211ae37a66be
Kurtiss Hare <kurtiss@gmail.com>
2010-04-01 01:15:00 -0700

    Adds shrapnel.security.tiny_guid for generating randomish tinyurl style identifiers

--- shrapnel/security.py
+++ shrapnel/security.py
@@ -10,6 +10,7 @@ Copyright (c) 2010 Medium Entertainment, Inc. All rights reserved.
 import base64
 import uuid as uuid_module
 import hashlib
+import random
 
 
 def uuid():
@@ -20,3 +21,38 @@ def hash(value):
 	
 def webencode(value):
 	return base64.b64encode(value, ('-', '_')).rstrip('=')
+	
+def tiny_guid(value, namespace = 'default', min_chars = 4):
+    characters = list('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    factors = [
+        839299365868340224,
+        13537086546263552,
+        218340105584896,
+        3521614606208,
+        56800235584,
+        916132832,
+        14776336,
+        238328,
+        3844,
+        62,
+        1
+    ]
+    offset_test = len(factors) - min_chars - 1
+
+    output = []
+
+    for (offset, factor) in enumerate(factors):
+        if value >= factor:
+            index = value / factor
+            output.append(index)
+            value -= index * factor
+        elif len(output) or offset > offset_test:
+            output.append(0)
+
+    for i in xrange(len(output) - 1, -1, -1):
+        column_characters = list(characters)
+        random.seed("pXe2eYsKTQyc6NKuKwJonQ:{0}:{1}".format(namespace, ''.join(output[i+1:])))
+        random.shuffle(column_characters)
+        output[i] = column_characters[output[i]]
+
+    return ''.join(output)    
