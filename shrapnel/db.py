1938d47e14c33fb39aee3d3320527b5dcee000bb
Stephen Altamirano <stephenaltamirano@stephen-altamiranos-macbook-pro.local>
2010-04-15 19:26:14 -0700

    adds itransaction decorator

--- shrapnel/db.py
+++ shrapnel/db.py
@@ -7,18 +7,60 @@ Created by Kurtiss Hare on 2010-02-22.
 Copyright (c) 2010 Medium Entertainment, Inc. All rights reserved.
 """
 
+import functools
 import security
 import string
 import tornado.database
+import types
+
+def transaction(retries=1):
+    def decorator(undecorated):
+        @functools.wraps(undecorated)
+        def decorated(*args, **kwargs):
+            generator = undecorated(*args, **kwargs)
+
+			if not type(generator) == types.GeneratorType:
+				raise RuntimeError("{0.__name__} does not conform to the 'transaction' protocol.  It must return a generator.".format(undecorated))
+
+			try:
+                db = generator.next()
+            except StopIteration:
+                db = None
+
+            if not type(db) == '':
+                raise RuntimeError('')
+
+            exception = None
+            result = None
+            for i in range(retries):
+                db.execute('begin')
+                try:
+                    try:
+                        result = generator.next()
+                    except StopIteration:
+                        exception = RuntimeError('')
+                        break
+
+                except Exception e:
+                    db.execute('rollback')
+                    generator = undecorated(*args, **kwargs)
+                    generator.next()
+                    exception = e
+                else:
+                    db.execute('commit')
+                    return result
+            raise exception
+        return decorated
+    return decorator
 
 
 class InList(object):
     def __init__(self, list):
         self.list = list
-    
+
     def __parameterize__(self, format_spec):
         return self.list, str(self)
-        
+
     def __str__(self):
         import itertools
         return "({0})".format(', '.join(itertools.repeat("%s", len(self.list))))
@@ -46,19 +88,19 @@ class Connection(object):
 
     def close(self):
         self.connection.close()
-        
+
     def reconnect(self):
         self.connection.reconnect()
 
     def iter(self, query, *format_args, **format_kwargs):
         return self._call_with_reconnect(self.connection.iter, query, format_args, format_kwargs)
-    
+
     def query(self, query, *format_args, **format_kwargs):
         return self._call_with_reconnect(self.connection.query, query, format_args, format_kwargs)
-        
+
     def get(self, query, *format_args, **format_kwargs):
         return self._call_with_reconnect(self.connection.get, query, format_args, format_kwargs)
-        
+
     def execute(self, query, *format_args, **format_kwargs):
         return self._call_with_reconnect(self.connection.execute, query, format_args, format_kwargs)
 
@@ -68,7 +110,7 @@ class Connection(object):
     def _call_with_reconnect(self, callable, query, format_args, format_kwargs):
         formatter = _ParameterizingFormatter()
         query = formatter.vformat(query, format_args, format_kwargs)
-        
+
         try:
             result = callable(query, *formatter.parameters)
         except tornado.database.OperationalError:
